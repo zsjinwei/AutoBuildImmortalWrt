5cdb7e66184592080347b501f9b656a4bf4550f9
huangjinwei <zsjinwei@foxmail.com>
2025-09-09 17:10:59 +0800

    增加虚拟机镜像编译, 其它定制化修改

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -54,9 +54,7 @@ PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 #24.10
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 # 文件管理器
